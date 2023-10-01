873d4565383cc718abaaa4fcc9ed7b30232b9225
Gregory Damiani <gregory.damiani@mobimeo.com>
2023-10-01 19:29:28 +0200

    basic python project setup, linting, testing, initial implementation of methods for the 4 command line switches.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ccwc",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "unittest",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
